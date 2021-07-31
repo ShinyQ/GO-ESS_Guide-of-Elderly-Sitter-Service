d7a24cd2e869491483d1129cb0b64234b46c841c
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2021-08-01 03:26:20 +0700

    Adding yml deployment file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         config(['app.locale' => 'id']);
         Carbon::setLocale('id');
     }
